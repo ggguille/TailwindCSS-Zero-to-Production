c8ab0219072b52f44547f04151bc7079d4958621
ggguille <g.granado.g@gmail.com>
2021-04-25 15:52:17 +0200

    define brand new colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: '#0fa9e6',
+        "brand-light": '#3fbaeb',
+        "brand-dark": '#0c87b8'
+      }
+    },
   },
   variants: {
     extend: {
